19da8b20ec491e5866622126dc9871457f4212fa
AhmedYousif6 <83282254+AhmedYousif6@users.noreply.github.com>
2024-10-01 02:53:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-#!/usr/bin/node
-
-const express = require('express');
-const router = require('./routes/index');
+import express from'express';
+import router from './routes/index';
 
 const server = express();
 const PORT = process.env.PORT ? process.env.PORT : 5000;
