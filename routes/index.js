9e9a2dd4231f5305c979079c7ca631e39db71fbc
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-10-01 17:30:05 -0700

    task done

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UsersController';
+import AuthController from '../controller/AuthController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UserController.postNew);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
 export default router;
